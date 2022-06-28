9f05ea9ded40242697fa13b4fdfb51293405db5e
Volodymyr Huk <nailz2kua@gmail.com>
2022-06-28 21:07:49 +0300

    change env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  converterApiKey: 'nW+IhPxH5hxLRD3RqITcfg==gDBORnpDthaBZ6HX',
+  converterApiUrl: 'https://api.api-ninjas.com/v1'
 };
